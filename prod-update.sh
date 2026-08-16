03cc7898c6e599fab4fd2ad96809cbb8a7560ad7
ikram3031 <mdikram.3031@gmail.com>
2026-08-16 16:57:26 +0600

    Update prod-update.sh target branch to live

--- prod-update.sh
+++ prod-update.sh
@@ -9,9 +9,9 @@ set -e
 PROJECT_DIR="/opt/monsuralitravels"
 cd "$PROJECT_DIR"
 
-echo "🔄 Pulling latest changes from master..."
-git fetch origin master
-git reset --hard origin/master
+echo "🔄 Pulling latest changes from live..."
+git fetch origin live
+git reset --hard origin/live
 
 echo "🏗️ Rebuilding and restarting containers..."
 docker compose -f docker-compose.prod.yml up -d --build
