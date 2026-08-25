fb8754e3dd85074c1e61578feadf1041dfbbeed7
ikram3031 <mdikram.3031@gmail.com>
2026-08-26 01:51:07 +0600

    TT06(feat): Create unified HeaderTitle component supporting General and Printables variations

--- dashboard/src/shared/components/layout/index.js
+++ dashboard/src/shared/components/layout/index.js
@@ -2,3 +2,4 @@ export { DashboardLayout, default } from './DashboardLayout';
 export { AppContainer } from './AppContainer';
 export { UnifiedSidebar } from './UnifiedSidebar';
 export { PageTitle } from './PageTitle';
+export { HeaderTitle, HeaderModeSwitcher } from '../common/HeaderTitle';
